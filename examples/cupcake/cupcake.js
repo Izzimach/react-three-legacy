c69599a940e4372cc72ad21b1b5e1de67a329e4d
Gary Haussmann <gjhaussmann@gmail.com>
2015-12-17 14:43:27 -0700

    Shouldn't need disableHotLoader for cupcake examples anymore

--- examples/cupcake/cupcake.js
+++ examples/cupcake/cupcake.js
@@ -53,14 +53,14 @@ var ExampleScene = React.createClass({
       {name:'maincamera', fov:'75', aspect:this.props.width/this.props.height, near:1, far:5000, position:new THREE.Vector3(0,0,600), lookat:new THREE.Vector3(0,0,0)});
 
     return (
-        React.createElement(ReactTHREE.Renderer, { disableHotLoader: true, width:this.props.width, height:this.props.height },
+        React.createElement(ReactTHREE.Renderer, { width:this.props.width, height:this.props.height },
             React.createElement(ReactTHREE.Scene,
-                {disableHotLoader:true, width:this.props.width, height:this.props.height, camera:'maincamera'}
+                {width:this.props.width, height:this.props.height, camera:'maincamera'}
                 ,MainCameraElement
                 ,React.createElement(Cupcake, this.props.cupcakedata)
             ),
             React.createElement(ReactTHREE.Scene,
-                {disableHotLoader:true, width:this.props.width, height:this.props.height, camera:'maincamera'}
+                {width:this.props.width, height:this.props.height, camera:'maincamera'}
                 ,MainCameraElement
                 ,React.createElement(Cupcake, this.props.cupcakedata2)
             )
